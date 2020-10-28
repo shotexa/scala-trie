52e62917d45c745e63818f132a09bc0417ccced5
Shota Jolbordi <shota.jolbordi@protonmail.com>
2020-10-28 06:51:21 +0400

    Add stringsStartingWith function, rename prefixesMatchingString, fix bugs

--- src/main/scala/mutable/Trie.scala
+++ src/main/scala/mutable/Trie.scala
@@ -5,20 +5,20 @@ import collection.mutable.{Map => MutMap}
 
 class Trie(elements: String*) {
 
-  elements.foreach(add)
-
-  val root = new Node
-
   class Node(
       var isWord: Boolean = false,
       val children: MutMap[Char, Node] = MutMap.empty
   )
 
+  elements.foreach(add)
+  lazy val root = new Node
+
   def add(word: String): Trie = {
     var current = root
-    
+
     for (char <- word)
       current = current.children.getOrElseUpdate(char, new Node)
+
     current.isWord = true
 
     this
@@ -37,7 +37,7 @@ class Trie(elements: String*) {
     current.isWord
   }
 
-  def prefixesMatchingString(word: String): Set[String] = {
+  def substringsOf(word: String): Set[String] = {
     var current = Option(root)
     val set     = Set.newBuilder[Int]
 
@@ -46,9 +46,29 @@ class Trie(elements: String*) {
       current = current.get.children.get(char)
 
     }
+    if (current.exists(_.isWord)) set += word.length
 
     set.result().map(word.slice(0, _))
 
+  }
+
+  def stringsStartingWith(prefix: String): Set[String] = {
+    var current = Option(root)
+    for (char <- prefix if current.nonEmpty)
+      current = current.get.children.get(char)
+
+    if (current.isEmpty) Set.empty[String]
+
+    val set = Set.newBuilder[String]
+
+    def walk(node: Node, path: Array[Char]): Unit = {
+      if (node.isWord) set += prefix + path.mkString
+      for ((k, v) <- node.children) walk(v, path :+ k)
+    }
+
+    walk(current.get, Array.empty[Char])
+
+    set.result()
   }
 
 }
